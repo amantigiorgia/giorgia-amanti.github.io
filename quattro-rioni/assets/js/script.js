dfb17a0c4627b1708712f04d2db2789e595a131e
GiorgiaAmanti <giorgia.amanti@studenti.unimi.it>
2025-12-13 12:17:42 +0100

    Move project to root and update JS/paths

--- quattro-rioni/assets/js/script.js
+++ quattro-rioni/assets/js/script.js
@@ -1,16 +1,15 @@
 
-
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("newsletterForm");
   const successMessage = document.getElementById("successMessage");
 
   form.addEventListener("submit", (event) => {
     event.preventDefault(); 
-
     successMessage.style.display = "block"; 
     form.reset(); 
   });
 });
+
 
 
 
